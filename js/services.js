dec847dcb4708c526e940b4f875bbd0442c81a6e
Nicholas <niku744@gmail.com>
2014-04-03 17:23:57 -0400

    Trying to get api working.

--- js/services.js
+++ js/services.js
@@ -3,5 +3,13 @@
  */
 'use strict';
 
-var bikeTrace= angular.module('bikeTrace.services', []);
+var bikeTrace= angular.module('bikeTrace.services', ["ngResource"]);
 
+bikeTrace.factory('getTraceData',function($resource){
+
+    return $resource(
+        "https://oauth2-api.mapmyapi.com/v7.0/route/",
+        {limit:20},
+    )
+
+});
